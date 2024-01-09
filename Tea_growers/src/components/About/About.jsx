5325850fac91320ddf8ae054fee65e00e6b84e9d
kibet nathan <nathanrufus540@gmail.com>
2024-01-09 17:28:43 +0300

    Done with About

--- Tea_growers/src/components/About/About.jsx
+++ Tea_growers/src/components/About/About.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./About.css";
+import tea from "../Assets/tea.avif";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <section className="about">
+        <h1>About</h1>
+        <div className="aboutcontent">
+          <img src={tea} alt="" />
+          <div className="aboutea">
+            <h2>Importance of Tea</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
+              vel illum! Omnis deserunt porro cupiditate officia. Excepturi
+              consectetur in aliquid, dolores odit adipisci perspiciatis natus
+              aut officia rem? Ratione, ab.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default About;
